99e0191a9649115f80a7aa0420680af03bc33370
YannisMarketakis <ymark_em@yahoo.gr>
2015-11-04 13:43:19 +0200

    added junit test for multiple instantiations

---
+++ src/test/java/eu/delving/x3ml/TestMultipleInstantiaton.java
@@ -0,0 +1,32 @@
+package eu.delving.x3ml;
+
+import static eu.delving.x3ml.AllTests.compareNTriples;
+import static eu.delving.x3ml.AllTests.document;
+import static eu.delving.x3ml.AllTests.engine;
+import static eu.delving.x3ml.AllTests.errorFree;
+import static eu.delving.x3ml.AllTests.xmlToNTriples;
+import eu.delving.x3ml.engine.Generator;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
+ * @author Nikos Minadakis (minadakn 'at' ics 'dot' forth 'dot' gr)
+ */
+public class TestMultipleInstantiaton {
+    private final Logger log = Logger.getLogger(getClass());
+    private final Generator VALUE_POLICY = X3MLGeneratorPolicy.load(null, X3MLGeneratorPolicy.createUUIDSource(1));
+
+    @Test
+    public void testEmptyElement() {
+        X3MLEngine engine = engine("/multiple_instances/mappings.x3ml");
+        X3MLEngine.Output output = engine.execute(document("/multiple_instances/input.xml"),VALUE_POLICY);
+        String[] mappingResult = output.toStringArray();
+        String[] expectedResult = xmlToNTriples("/multiple_instances/expectedResult.rdf");
+        List<String> diff = compareNTriples(expectedResult, mappingResult);
+        assertTrue("\nLINES:"+ diff.size() + "\n" + StringUtils.join(diff, "\n") + "\n", errorFree(diff));
+    }   
+}
