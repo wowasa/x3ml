0ed78a55bdb62fcace4fbd739a582f9a22ee72ee
YannisMarketakis <ymark_em@yahoo.gr>
2015-11-19 14:35:14 +0200

    minor: removed unused imports

--- src/test/java/eu/delving/x3ml/TestExceptionMessages.java
+++ src/test/java/eu/delving/x3ml/TestExceptionMessages.java
@@ -1,13 +1,9 @@
 package eu.delving.x3ml;
 
-import static eu.delving.x3ml.AllTests.compareNTriples;
 import static eu.delving.x3ml.AllTests.document;
 import static eu.delving.x3ml.AllTests.engine;
-import static eu.delving.x3ml.AllTests.errorFree;
 import static eu.delving.x3ml.AllTests.xmlToNTriples;
 import eu.delving.x3ml.engine.Generator;
-import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
