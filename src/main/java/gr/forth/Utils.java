fc7129b16d2f50f06df509910b3e6e05dffabc97
YannisMarketakis <ymark_em@yahoo.gr>
2015-12-01 16:45:42 +0200

    Created a new method for producing an exception message (that is caught and reported as error), when empty elements occur (issue #11)

--- src/main/java/gr/forth/Utils.java
+++ src/main/java/gr/forth/Utils.java
@@ -8,11 +8,27 @@ import eu.delving.x3ml.engine.X3ML;
  */
 public class Utils {
     public static String produceLabelGeneratorMissingArgumentError(X3ML.GeneratorElement generator, String expectedValue){
-        return new StringBuilder().append("The attribute ")
+        return new StringBuilder().append("LabelGenerator Error: ")
+                                  .append("The attribute ")
                                   .append("\"")
                                   .append(expectedValue)
                                   .append("\"")
                                   .append(" is missing from the generator. ")
+                                  .append("[Mapping #: ")
+                                  .append(X3ML.RootElement.mappingCounter)
+                                  .append(", Link #: ")
+                                  .append(X3ML.RootElement.linkCounter)
+                                  .append("]. ")
+                                  .append(generator).toString();
+    }
+    
+    public static String produceLabelGeneratorEmptyArgumentError(X3ML.GeneratorElement generator, String attrValue){
+        return new StringBuilder().append("LabelGenerator Error: ")
+                                  .append("The argument with name ")
+                                  .append("\"")
+                                  .append(attrValue)
+                                  .append("\"")
+                                  .append(" does not containg any value. ")
                                   .append("[Mapping #: ")
                                   .append(X3ML.RootElement.mappingCounter)
                                   .append(", Link #: ")
