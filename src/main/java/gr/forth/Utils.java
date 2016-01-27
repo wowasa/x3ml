5dba9bee9e603aa5c7d4cf67cfd3bcd07f92a503
YannisMarketakis <ymark_em@yahoo.gr>
2016-01-27 14:50:13 +0200

    added a new dependency for xml combine (reverted from commit d51c3fe672db156723eae7d8cb74eaaf6cc1b312)

--- src/main/java/gr/forth/Utils.java
+++ src/main/java/gr/forth/Utils.java
@@ -20,15 +20,12 @@ package gr.forth;
 
 import static eu.delving.x3ml.X3MLEngine.exception;
 import eu.delving.x3ml.engine.X3ML;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import org.apache.commons.io.IOUtils;
+import org.atteo.xmlcombiner.XmlCombiner;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
@@ -73,21 +70,19 @@ public class Utils {
         }
     }
     
-    public static Element parseMultipleXMLFiles(List<File> xmlFiles) throws SAXException, ParserConfigurationException, IOException{
-        StringBuilder inputBuilder=new StringBuilder();
+    public static Element parseMultipleXMLFiles(List<File> xmlFiles){
         try{
+            XmlCombiner combiner = new XmlCombiner();
             for(File file : xmlFiles){
-                BufferedReader br=new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
-                String line;
-                while((line=br.readLine())!=null){
-                    inputBuilder.append(line).append("\n");
-                }
+                combiner.combine(new FileInputStream(file));
             }
+            return combiner.buildDocument().getDocumentElement();
+        }catch(ParserConfigurationException ex){
+            throw exception("an error occurred while initializing xml combiner (for multiple files)",ex);
         }catch(IOException ex){
-            throw exception("Unable to read input file ",ex);
+            throw exception("an error occured with the given input files", ex);
+        }catch(SAXException ex){
+            throw exception("an error occured while parsing XML input files",ex);
         }
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setNamespaceAware(true);
-        return factory.newDocumentBuilder().parse(IOUtils.toInputStream(inputBuilder.toString())).getDocumentElement();
     }
 }
