112a6755ed50f76f50e5783363531646a93dd1d3
YannisMarketakis <ymark_em@yahoo.gr>
2016-07-22 11:51:45 +0300

    added a Logger for reporting error messages

--- src/main/java/gr/forth/Utils.java
+++ src/main/java/gr/forth/Utils.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 import javax.xml.parsers.ParserConfigurationException;
+import org.apache.log4j.Logger;
 import org.atteo.xmlcombiner.XmlCombiner;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
@@ -33,6 +34,8 @@ import org.xml.sax.SAXException;
  * @author Nikos Minadakis (minadakn 'at' ics 'dot' forth 'dot' gr)
  */
 public class Utils {
+    private static final Logger LOGGER=Logger.getLogger(Utils.class);
+    
     public static String produceLabelGeneratorMissingArgumentError(X3ML.GeneratorElement generator, String expectedValue){
         return new StringBuilder().append("LabelGenerator Error: ")
                                   .append("The attribute ")
@@ -65,7 +68,7 @@ public class Utils {
     
     public static void printErrorMessages(String ... messages){
         for(String msg : messages){
-            System.out.println(msg);
+            LOGGER.error(msg);
         }
     }
     
