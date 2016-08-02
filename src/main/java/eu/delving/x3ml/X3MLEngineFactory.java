f2d6fd021d4d8c0e56cb05dfee3637fe8d475b3e
YannisMarketakis <ymark_em@yahoo.gr>
2016-08-02 13:23:40 +0300

    added the skeleton of the new class

---
+++ src/main/java/eu/delving/x3ml/X3MLEngineFactory.java
@@ -0,0 +1,68 @@
+package eu.delving.x3ml;
+
+/*==============================================================================
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+==============================================================================*/
+
+/** This class instantiates X3ML Engine and handles the ingestion of user-defined details 
+ * in a straightforward manner. Furthermore it can execute the X3ML engine for producing 
+ * the desired output.
+ *
+ * @author Yannis Marketakis <marketak@ics.forth.gr>
+ * @author Nikos Minadakis <minadakn@ics.forth.gr>
+ */
+public class X3MLEngineFactory {
+    
+    public static X3MLEngineFactory createX3MLEngine(){
+        return null;
+    }
+    
+    public static X3MLEngineFactory withMappings(){
+        return null;
+    }
+    
+    public static X3MLEngineFactory withInputFiles(){
+        return null;
+    }
+    
+    public static X3MLEngineFactory withInputFolder(){
+        return null;
+    }
+    
+    public static X3MLEngineFactory withGeneratorPolicy(){
+        return null;
+    }
+    
+    public static X3MLEngineFactory withUuidSize(){
+        return null;
+    }
+    
+    public static X3MLEngineFactory withOutput(){
+        return null;
+    }
+    
+    /*for general details (i.e. associaton table)*/
+    public static X3MLEngineFactory with(){
+        return null;
+    }
+    
+    public static void execute(){
+        
+    }
+
+}
