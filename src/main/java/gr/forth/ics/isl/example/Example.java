bedcf88590b5164a99584ec950968d1d4bd5e815
YannisMarketakis <ymark_em@yahoo.gr>
2016-08-04 13:16:12 +0300

    moved example classes to a proper place and added more examples (to be used from the Factory client)

--- src/main/java/gr/forth/ics/isl/example/Example.java
+++ src/main/java/gr/forth/ics/isl/example/Example.java
@@ -16,7 +16,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 ==============================================================================*/
-package gr.forth;
+package gr.forth.ics.isl.example;
 
 import eu.delving.x3ml.X3MLEngine;
 import static eu.delving.x3ml.X3MLEngine.exception;
