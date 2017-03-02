dd6c2b9d5fd91f5da5116a8f6f790ae695e74579
YannisMarketakis <ymark_em@yahoo.gr>
2017-03-02 16:43:43 +0200

    minor improvements in junit test (removed unwanted assertions, renamed resource files)

--- src/test/java/eu/delving/x3ml/TestFullNamespace.java
+++ src/test/java/eu/delving/x3ml/TestFullNamespace.java
@@ -37,14 +37,13 @@ public class TestFullNamespace {
     private final Generator VALUE_POLICY = X3MLGeneratorPolicy.load(null, X3MLGeneratorPolicy.createUUIDSource(1));
 
     @Test
-    public void testEmptyElement() {
+    public void testFullUris() {
         X3MLEngine engine = engine("/full_namespace/01-coin-simple.x3ml");
         X3MLEngine.Output output = engine.execute(document("/full_namespace/00-coin-input.xml"),VALUE_POLICY);
         String[] mappingResult = output.toStringArray();
-        String[] expectedResult = xmlToNTriples("/full_namespace/01-coin-simple-rdf.xml");
+        String[] expectedResult = xmlToNTriples("/full_namespace/01-coin-simple.rdf");
         List<String> diff = compareNTriples(expectedResult, mappingResult);
         assertTrue("\nLINES:"+ diff.size() + "\n" + StringUtils.join(diff, "\n") + "\n", errorFree(diff));
-        assertTrue(true);
     }
     
     @Test
@@ -56,6 +55,4 @@ public class TestFullNamespace {
         List<String> diff = compareNTriples(expectedResult, mappingResult);
         assertTrue("\nLINES:"+ diff.size() + "\n" + StringUtils.join(diff, "\n") + "\n", errorFree(diff));
     }
-
-
 }
