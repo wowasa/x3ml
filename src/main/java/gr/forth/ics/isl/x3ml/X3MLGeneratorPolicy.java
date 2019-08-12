b45adf3e0d2d2695f409e96da6626bf50d2673d0
YannisMarketakis <ymark_em@yahoo.gr>
2019-08-12 11:18:20 +0300

    re-disabled slash character encoding

--- src/main/java/gr/forth/ics/isl/x3ml/X3MLGeneratorPolicy.java
+++ src/main/java/gr/forth/ics/isl/x3ml/X3MLGeneratorPolicy.java
@@ -314,7 +314,7 @@ public class X3MLGeneratorPolicy implements Generator {
                 }
             }
             return uriValue(namespaceUri + uriTemplate.expand()
-//                                                                 .replaceAll(Labels.SLASH_CHARACTER_ENCODED, Labels.SLASH_CHARACTER)
+                                                               .replaceAll(Labels.SLASH_CHARACTER_ENCODED, Labels.SLASH_CHARACTER)
                                                                .replaceAll(Labels.PERCENT_CHARACTER_ENCODED, Labels.PERCENT_CHARACTER)
                                                           
             );
